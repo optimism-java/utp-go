1b02c96550b7aea14fbfcc2996a7531b89911c7d
thinkAfCod <q315xia@163.com>
2024-03-05 22:53:13 +0800

    fix: seq num

--- libutp/utp.go
+++ libutp/utp.go
@@ -2364,14 +2364,9 @@ func (mx *SocketMultiplexer) processIncoming(conn *Socket, packet []byte, syn bo
 	// packet this is. ackNum is the last acked, seqNum is the
 	// current. Subtracting 1 makes 0 mean "this is the next
 	// expected packet".
-	var seqNum uint16
-	if pkSeqNum < conn.ackNum-1 {
-		seqNum = (conn.ackNum - 1 - pkSeqNum) & seqNumberMask
-	} else {
-		seqNum = (pkSeqNum - conn.ackNum - 1) & seqNumberMask
-	}
+	seqNum := (pkSeqNum - conn.ackNum - 1) & seqNumberMask
 
-	mx.logger.Debug("seq_num = (pk_seq_num - conn_ack_num - 1) & 0xFFFF", zap.Uint16("seq_num", seqNum))
+	mx.logger.Debug("seq_num = (pk_seq_num - conn_ack_num - 1) & 0xFFFF", zap.Uint16("seq_num", seqNum), zap.Uint16("pk_seq_num", pkSeqNum), zap.Uint16("conn_ack_num", conn.ackNum))
 	// Getting an invalid sequence number?
 	if seqNum >= reorderBufferMaxSize {
 		if seqNum >= (seqNumberMask+1)-reorderBufferMaxSize && pkFlags != stState {
